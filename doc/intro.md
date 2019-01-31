842302e5430fe073adcf118f434f1d334e27d71d
Roman Gerasimov <gr.evocatus@gmail.com>
2019-01-31 15:07:00 +0300

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to honeysql-example
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/honeysql_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns honeysql-example.core-test
+  (:require [clojure.test :refer :all]
+            [honeysql-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
